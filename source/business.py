a6973431983d8fb0fc675676dc3f4943698f3a01
Gerdie <Gerdie@users.noreply.github.com>
2015-05-12 18:08:17 -0400

    Update business.py
    I fix

--- source/business.py
+++ source/business.py
@@ -1,18 +1,18 @@
 class Business_card(object):
-	#Defines the Business Card class. id: intiger id of card for shuffling purposes, possesion: if a player posses this card, set int to player number, special: special card ability
-	def __init__(self, id, name, cost, income, consumer_type, pr_problem, special, possesion):
+	#Defines the Business Card class. id: intiger id of card for shuffling purposes, possession: if a player posses this card, set int to player number, special: special card ability
+	def __init__(self, id, name, cost, income, consumer_type, pr_problem, special, possession):
 		self.name = name
 		self.cost = cost
 		self.income = income
-		self.cosumer_type = consumer_type
+		self.consumer_type = consumer_type
 		self.pr_problem = pr_problem
-		self.possesion = possesion
+		self.possession = possession
 
 #definition of all Business cards
 
 
 greenwash_consulting = Business_card(1, "GREENWASH CONSULTING", 6, 8, ["Green"], [], ["Lobbyist"], 0)
-natural_beauty_rhinoplasty = Business_card(2, "NATURAL BEAUTY RHINOPLASTY", 8, 6, ["Luxery", "Health"], [], [], 0)
+natural_beauty_rhinoplasty = Business_card(2, "NATURAL BEAUTY RHINOPLASTY", 8, 6, ["Luxury", "Health"], [], [], 0)
 faux_news = Business_card(3, "FAUX  NEWS", 5, 4, ["Media"], [], ["Lobbyist", "Sponsered"] 0)
 start_worrying_insurance = Business_card(4, "START WORRYING INSURANCE", 4, 4, ["Finance", "Health"], [], [], 0)
 the_liberal_media = Business_card(5, "THE LIBERAL MEDIA", 5, 4, ["Media"], [], ["Lobbyist", "Sponsered"], 0)
@@ -22,7 +22,7 @@ pharmopticon = Business_card(8, "PHARMOPTICON", 10, 6, ["Technology", "Health"],
 happy_ending_pictures = Business_card(9, "HAPPY ENDING PICTURES", 4, 8, ["Entertainment"], [], [], 0)
 bank_of_shamerica = Business_card(10, "BANK OF SHAMERICA", 4, 4, ["Finance", "Home"], [], [], 0)
 genetifood = Business_card(11, "GENETIFOOD", 10, 6, ["Technology", "Food", [], []. 0)
-big_journey_suvs = Business_card(12, "BIG JOURNEY SUVS", 12, 8, ["Transportation", "Luxery"], ["Polluting"], [], 0)
+big_journey_suvs = Business_card(12, "BIG JOURNEY SUVS", 12, 8, ["Transportation", "Luxury"], ["Polluting"], [], 0)
 mud_hole_acupuncture = Business_card(13, "MUD HOLE ACUPUNCTURE", 5, 4, ["Health", "Green"], [], [] 0)
 lights_camera_megachurch = Business_card(14, "LIGHTS CAMERA MEGACHURCH", 5, 4, ["Media"], [], ["Lobbyist", "Sponsered"], 0)
 chupadinero_casino = Business_card (15, "CHUPADINERO CASINO", 10, 5, ["Entertainment", "Finance", "Sin"], [], [], 0)
@@ -30,7 +30,7 @@ sweatibank = Business_card(16, "SWEATIBANK", 4, 8, ["Entertainment"], [], [], 0)
 start_up_labs = Business_card(17, "START UP LABS", 15, 4, ["Technology", "Finance"], [], ["Job Creater"], 0)
 heavy_lid_dispensaries = Business_card(18, "HEAVY LID DISPENSARIES", 4, 5, ["Health", "Sin"], [], [], 0)
 pantent_trolls = Business_card(19, "PATENT TROLLS", 4, 6, ["Finance"], [], [], 0)
-naive_bottled_water = Business_card(20, "NAIVE BOTTLED WATER", 8, 6, ["Luxery", "Food"], ["Polluting"], [], 0)
+naive_bottled_water = Business_card(20, "NAIVE BOTTLED WATER", 8, 6, ["Luxury", "Food"], ["Polluting"], [], 0)
 factory_farms = Business_card(21, "FACTORY FARMS", 4, 10, ["Food"], ["Polluting", "Labor"], [], 0)
 back_alley_business_models = Business_card(22, "BACK ALLEY BUSINESS MODELS", 4, 6, ["Finance"], [], ["Think Tank"], 0)
 soal_of_coal = Business_card(23, "SOAL OF COAL", 12, 8, ["Energy", "Home"], ["Polluting"], [], 0)
